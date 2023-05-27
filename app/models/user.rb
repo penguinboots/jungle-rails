b27d8d6ae15f741b6d5dcfdf9d247b45c7785d72
penguinboots <sabrina.ynw@gmail.com>
2023-05-27 11:22:04 -0700

    refactor: use User class method to auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
-
   has_secure_password
 
-	# Verify that email field is not blank and that it doesn't already exist in the db (prevents duplicates):
-	validates :email, presence: true, uniqueness: true
+  # Verify that email field is not blank and that it doesn't already exist in the db (prevents duplicates):
+  validates :email, presence: true, uniqueness: true
 
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by(email: email.downcase.strip)
+    if @user && @user.authenticate(password)
+      return @user
+    else
+      return nil
+    end
+  end
 end
