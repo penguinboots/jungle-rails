4f5bc0f9d9564334c355e715e9a19a9b064a6d89
penguinboots <sabrina.ynw@gmail.com>
2023-05-27 11:33:04 -0700

    feat: add User class validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,14 @@ class User < ApplicationRecord
   has_secure_password
 
   # Verify that email field is not blank and that it doesn't already exist in the db (prevents duplicates):
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password,
+            presence: true,
+            confirmation: true,
+            length: {
+              minimum: 5
+            }
+  validates :password_confirmation, presence: true
 
   def self.authenticate_with_credentials(email, password)
     @user = User.find_by(email: email.downcase.strip)
